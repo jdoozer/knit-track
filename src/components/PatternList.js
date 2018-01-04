aa448c9394622f8630e2cd693d8cb4989da32800
Joanna Cohen <joanna.cohen@gmail.com>
2018-01-03 23:42:24 -0800

    added display message for startup case with no patterns

--- src/components/PatternList.js
+++ src/components/PatternList.js
@@ -2,29 +2,48 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import List from 'material-ui/List';
+import Typography from 'material-ui/Typography';
 import ContentHeader from 'components/ContentHeader';
 import PatternTitleBlock from 'components/PatternTitleBlock';
 
-const styles = () => ({
+const styles = theme => ({
   list: {
     padding: 0,
   },
+  noPattern: {
+    marginTop: theme.spacing.unit * 2,
+  },
 });
 
-const PatternList = ({ patterns, onPatternClick, classes }) => (
-  <div>
-    <ContentHeader>Pattern List</ContentHeader>
-    <List className={classes.list}>
-      {patterns.map(pattern => (
-        <PatternTitleBlock
-          key={pattern.patternID}
-          {...pattern}
-          onClick={() => onPatternClick(pattern.patternID)}
-        />
-      ))}
-    </List>
-  </div>
-);
+const PatternList = ({ patterns, onPatternClick, classes }) => {
+
+  let patternListContent = (
+    <Typography type="subheading" className={classes.noPattern}>
+      No patterns created yet!
+    </Typography>
+  );
+
+  if (patterns.length) {
+    patternListContent = (
+      <List className={classes.list}>
+        {patterns.map(pattern => (
+          <PatternTitleBlock
+            key={pattern.patternID}
+            {...pattern}
+            onClick={() => onPatternClick(pattern.patternID)}
+          />
+        ))}
+      </List>
+    );
+  }
+
+  return(
+    <div>
+      <ContentHeader>Pattern List</ContentHeader>
+      {patternListContent}
+    </div>
+  );
+};
 
 PatternList.propTypes = {
   patterns: PropTypes.arrayOf(
