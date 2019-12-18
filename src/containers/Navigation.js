9eff29ee892db00bbb89c60a7428fe3a3a44ce37
Joanna Cohen <joanna.cohen@gmail.com>
2019-12-17 17:53:03 -0800

    set Navigation bar to load patterns correctly on log in

--- src/containers/Navigation.js
+++ src/containers/Navigation.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { subscribePatternList, clearError } from 'actions';
-import { getPatternTitlesSorted, getPatternsLoading, getPatternsError } from 'reducers';
-// import ProgressModal from 'components/ProgressModal';
+import {
+  getPatternTitlesSorted, getPatternsLoading, getPatternsError
+} from 'reducers';
 import ErrorSnackbar from 'components/ErrorSnackbar';
 import NavDrawer from 'components/NavDrawer';
 
@@ -18,47 +19,53 @@ const mapDispatchToProps = {
   clearError: () => clearError('patterns')
 };
 
-class Navigation extends React.Component {
 
-  componentDidMount() {
-    if (!this.props.placeholder)
-      this.props.subscribePatternList(true);
-  }
+const Navigation = (props) => {
 
-  componentWillUnmount() {
-    this.props.subscribePatternList(false);
-  }
+  const {
+    loading,
+    error,
+    patternTitles,
+    placeholder,
+    mobileOpen,
+    handleDrawerToggle,
+    subscribePatternList,
+    clearError
+  } = props;
 
-  render() {
+  useEffect(() => {
+    if (!placeholder)
+      subscribePatternList(true);
+    return (() => {
+      subscribePatternList(false);
+    });
+  }, [placeholder, subscribePatternList]);
 
-    const {
-      loading, error, patternTitles, placeholder, mobileOpen, handleDrawerToggle
-    } = this.props;
-
-    return (
-      <>
-        <NavDrawer
-          placeholder={placeholder}
-          patternTitles={patternTitles}
-          mobileOpen={mobileOpen}
-          handleDrawerToggle={handleDrawerToggle}
-          loadingPatterns={loading}
-        />
-        <ErrorSnackbar open={error && !loading} onClose={clearError}>
-          Error loading patterns!
-        </ErrorSnackbar>
-      </>
-    );
-  }
+  return (
+    <>
+      <NavDrawer
+        placeholder={placeholder}
+        patternTitles={patternTitles}
+        mobileOpen={mobileOpen}
+        handleDrawerToggle={handleDrawerToggle}
+        loadingPatterns={loading}
+      />
+      <ErrorSnackbar open={error && !loading} onClose={clearError}>
+        Error loading patterns!
+      </ErrorSnackbar>
+    </>
+  );
 }
 
 Navigation.propTypes = {
-  patternTitles: PropTypes.array.isRequired,
   loading: PropTypes.bool.isRequired,
   error: PropTypes.bool.isRequired,
+  patternTitles: PropTypes.array.isRequired,
+  placeholder: PropTypes.bool.isRequired,
+  mobileOpen: PropTypes.bool.isRequired,
+  handleDrawerToggle: PropTypes.func.isRequired,
   subscribePatternList: PropTypes.func.isRequired,
   clearError: PropTypes.func.isRequired,
-  placeholder: PropTypes.bool.isRequired
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Navigation);
