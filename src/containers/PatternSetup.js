673e56a1141e5d125b3cd8059418da68e8d6a36a
Joanna Cohen <joanna.cohen@gmail.com>
2020-01-04 17:06:43 -0800

    add redirect to create pattern thunk instead of using lastCreatedId in store

--- src/containers/PatternSetup.js
+++ src/containers/PatternSetup.js
@@ -1,56 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { createPattern, clearLastCreated, clearError } from 'actions';
-import {
-  getLastCreatedPatternId, getPatternsLoading, getPatternsError
-} from 'reducers';
+import { createPattern, clearError } from 'actions';
+import { getPatternsLoading, getPatternsError } from 'reducers';
 import PatternForm from 'components/PatternForm';
 
 const mapStateToProps = state => ({
-  lastCreatedId: getLastCreatedPatternId(state),
   loading: getPatternsLoading(state),
   error: Boolean(getPatternsError(state)),
 });
 
-const mapDispatchToProps = {
-  createPattern: patternData => createPattern(patternData),
-  clearError: () => clearError('patterns'),
-  clearLastCreated: () => clearLastCreated('patterns'),
-};
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  createPattern: patternData => dispatch(createPattern(patternData))
+    .then(action => (
+      action.payload.error
+        ? null
+        : ownProps.history.push(`/patterns/${action.payload.pattern.patternId}`))
+    ),
+  clearError: () => dispatch(clearError('patterns')),
+});
 
-class PatternSetup extends React.Component {
-
-  componentDidUpdate() {
-    if (this.props.lastCreatedId) {
-      this.props.history.push(`/patterns/${this.props.lastCreatedId}`);
-    }
-  }
-
-  componentWillUnmount() {
-    this.props.clearLastCreated();
-  }
-
-  render() {
-    const { createPattern, clearError, loading, error } = this.props;
-    return (
-      <PatternForm
-        onSubmit={createPattern}
-        clearError={clearError}
-        loading={loading}
-        error={error}
-      />
-    );
-  }
-
-}
+const PatternSetup = ({ createPattern, clearError, loading, error }) => (
+  <PatternForm
+    onSubmit={createPattern}
+    clearError={clearError}
+    loading={loading}
+    error={error}
+  />
+);
 
 PatternSetup.propTypes = {
-  lastCreatedId: PropTypes.string.isRequired,
   loading: PropTypes.bool.isRequired,
   error: PropTypes.bool.isRequired,
   createPattern: PropTypes.func.isRequired,
-  clearLastCreated: PropTypes.func.isRequired,
+  clearError: PropTypes.func.isRequired,
   history: PropTypes.shape({
     push: PropTypes.func.isRequired,
   }).isRequired,
